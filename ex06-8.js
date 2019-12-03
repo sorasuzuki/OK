844b8e492ab7b23aaf0873d671d1593dd78cc0cc
sorasuzuki <zm18439@josai.ac.jp>
2019-12-03 02:56:13 +0000

    1203

--- ex06-8.js
+++ ex06-8.js
@@ -1,3 +1,3 @@
-function sayhello68(){alert('Hello,'+documet.getElementById('who').innerHTML+'!');}
-function taro68(){document.getElementById('who').innerHTML='太郎';}
-function hanako68(){document.getElementById('who').innerHTML='花子';}
+function sayhello68(){alert('Hello,'+documet.getElementById('who68').innerHTML+'!');}
+function taro68(){document.getElementById('who68').innerHTML='太郎';}
+function hanako68(){document.getElementById('who68').innerHTML='花子';}
