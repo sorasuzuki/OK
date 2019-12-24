bed18bbc3f3a49ea1b0997b3b411ee54e1ab5a24
sorasuzuki <zm18439@josai.ac.jp>
2019-12-24 03:09:21 +0000

    1224

--- ex06-10.js
+++ ex06-10.js
@@ -1,8 +1,7 @@
 function hyoji(){
     var str="一文字ずつ表示します。";
     var cnt=document.timer.moji.value.length;
-if(cnt<11){
-    document.timer.moji.value=str.substr(0,cnt+1);}
+if(cnt<11){document.timer.moji.value=str.substr(0,cnt+1);}
 else{document.timer.moji.value="";}}
 function startfnc(){setInterval("hyoji()",1000);}
 
